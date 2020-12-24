c23d0bd1a1d92861aa9d630d55a6d8919cdc8619
amitB-1 <58851858+amitB-1@users.noreply.github.com>
2020-12-24 10:02:52 +0200

    fixes and additions

--- src/app/bottom-bar/bottom-bar.component.ts
+++ src/app/bottom-bar/bottom-bar.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BottomBarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
   }
